1efa1912be53e0abdab5e1eb430035ddcfa5c4b9
Nazmul1582 <nazmulhasan1582@gmail.com>
2023-11-30 22:25:51 +0600

    applied verifyHR middleware for privant pay salary

---
+++ src/middlewares/verifyHR.js
@@ -0,0 +1,13 @@
+const User = require("../models/User");
+
+const verifyHR = async(req, res, next) => {
+    const email = req.decoded.email;
+    const query = { email: email }
+    const user = await User.findOne(query);
+    if(user?.userRole !== "admin"){
+        return res.status(403).send({message: "Forbidden access"})
+    }
+    next();
+}
+
+module.exports = verifyHR;
--- src/routes/users/index.js
+++ src/routes/users/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const { getUsers, createUser, verifyUser, getSingleUser, paySalary, getSalaries, firedUser } = require("../../api/users/controllers");
 const verifyToken = require("../../middlewares/verifyToken");
 const verifyAdmin = require("../../middlewares/verifyAdmin");
+const verifyHR = require("../../middlewares/verifyHR");
 const router = express.Router();
 
 router.get("/users", verifyToken, getUsers);
@@ -9,7 +10,7 @@ router.get("/users/:id", verifyToken, getSingleUser)
 router.get("/users/employee/salaries", verifyToken, getSalaries)
 
 router.post("/users", createUser)
-router.post("/users/employee/pay-salary", verifyToken, paySalary)
+router.post("/users/employee/pay-salary", verifyToken, verifyHR, paySalary)
 
 router.patch("/users/:id", verifyToken, verifyUser)
 router.patch("/users/admin/:id", verifyToken, verifyAdmin, firedUser)
